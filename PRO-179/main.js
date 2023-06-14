4479bc45629d118b88758002af72658cbc845add
Sangshraydas <74186921+Sangshraydas@users.noreply.github.com>
2023-06-14 15:29:10 +0530

    Add files via upload

---
+++ PRO-179/main.js
@@ -0,0 +1,30 @@
+let latitude=22.7868542, longitude=88.3643296;
+
+// Initializing Mapbox
+mapboxgl.accessToken = 'pk.eyJ1IjoiYXBvb3J2ZWxvdXMiLCJhIjoiY2ttZnlyMDgzMzlwNTJ4a240cmEzcG0xNyJ9.-nSyL0Gy2nifDibXJg4fTA';
+
+var map = new mapboxgl.Map({
+	container: 'map',
+	style: 'mapbox://styles/mapbox/streets-v11',
+	center: [longitude, latitude],
+	zoom: 16
+});   
+
+var img1=document.querySelector("#amber")
+
+//Create a Amber Palace, Jaipur Marker and add it to the map.
+var marker1=new mapboxgl.Marker({
+	element:img1
+})
+.setLngLat([75.85133, 2.98547])
+.addTo(map);
+
+map.addControl(
+	new MapboxGeocoder({
+		accessToken: mapboxgl.accessToken,
+		mapboxgl: mapboxgl
+	}).on('result',function(e){
+		destination=e.result.center
+	})
+);
+   
